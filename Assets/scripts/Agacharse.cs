9d6ee53b070a3cca65519f4bce8297b3c37acd75
PapiAlex18 <teambyloco18@gmail.com>
2024-12-18 11:18:59 +0100

    escena 2

--- Assets/scripts/Agacharse.cs
+++ Assets/scripts/Agacharse.cs
@@ -5,17 +5,14 @@ public class MovimientoYAgachado : MonoBehaviour
     public float alturaAgachado = 0.5f;  // La altura a la que el personaje se agacha
     public float velocidadCambio = 5f;   // Velocidad a la que el personaje se agacha y se levanta
     public Transform camara;             // Referencia a la cámara para ajustar su altura
-    public CapsuleCollider colision;     // El collider del personaje para ajustarlo al agacharse
+    public BoxCollider colision;     // El collider del personaje para ajustarlo al agacharse
 
-    private float alturaOriginal;        // Altura original del personaje
     private Vector3 posicionOriginalCamara; // Posición original de la cámara
 
     private bool estaAgachado = false;   // Determina si el personaje está agachado o no
 
     void Start()
     {
-        // Guardamos la altura original y la posición de la cámara al principio
-        alturaOriginal = colision.height;
         posicionOriginalCamara = camara.localPosition;
     }
 
@@ -37,18 +34,18 @@ public class MovimientoYAgachado : MonoBehaviour
         {
             // Reducir la escala del personaje y del collider
             transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1f, alturaAgachado, 1f), velocidadCambio * Time.deltaTime);
-            colision.height = Mathf.Lerp(colision.height, alturaAgachado, velocidadCambio * Time.deltaTime);
+            colision.size = new Vector3(colision.size.x, Mathf.Lerp(colision.size.y, alturaAgachado, velocidadCambio * Time.deltaTime),colision.size.z);
             colision.center = new Vector3(colision.center.x, alturaAgachado / 2f, colision.center.z);
 
             // Cambiar la posición de la cámara
-            camara.localPosition = Vector3.Lerp(camara.localPosition, new Vector3(camara.localPosition.x, posicionOriginalCamara.y - (alturaOriginal - alturaAgachado) / 2f, camara.localPosition.z), velocidadCambio * Time.deltaTime);
+            camara.localPosition = Vector3.Lerp(camara.localPosition, new Vector3(camara.localPosition.x, posicionOriginalCamara.y - (1 - alturaAgachado) / 2f, camara.localPosition.z), velocidadCambio * Time.deltaTime);
         }
         else
         {
             // Si no está agachado, volvemos a la posición original
-            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1f, alturaOriginal, 1f), velocidadCambio * Time.deltaTime);
-            colision.height = Mathf.Lerp(colision.height, alturaOriginal, velocidadCambio * Time.deltaTime);
-            colision.center = new Vector3(colision.center.x, alturaOriginal / 2f, colision.center.z);
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1f, 1, 1f), velocidadCambio * Time.deltaTime);
+            colision.size = new Vector3(colision.size.x, Mathf.Lerp(colision.size.y, 1, velocidadCambio * Time.deltaTime),colision.size.z);
+            colision.center = new Vector3(colision.center.x, 1 / 2f, colision.center.z);
 
             // Volver a la posición original de la cámara
             camara.localPosition = Vector3.Lerp(camara.localPosition, posicionOriginalCamara, velocidadCambio * Time.deltaTime);
